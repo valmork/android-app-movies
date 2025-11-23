87e81c072aea27d90fdc99f5c51ba07c530f9c49
anton <korolyov.anton0208@gmail.com>
2025-11-23 21:44:40 +0300

    добавлена первая папка movies

---
+++ Movies/app/src/main/java/com/example/movies/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.movies;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.core.Scheduler;
+import io.reactivex.rxjava3.functions.Consumer;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        ApiFactory.apiService.loadMovies()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<MovieResponse>() {
+                    @Override
+                    public void accept(MovieResponse movieResponse) throws Throwable {
+                        Log.d("MainActivity", movieResponse.toString());
+                    }
+                }, new Consumer<Throwable>() {
+                    @Override
+                    public void accept(Throwable throwable) throws Throwable {
+                        Log.d("MainActivity", throwable.toString());
+                    }
+                });
+    }
+}
---
+++ Movies/app/src/main/java/com/example/movies/Rating.java
@@ -0,0 +1,25 @@
+package com.example.movies;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Rating {
+
+    @SerializedName("kp")
+    private String kp;
+
+
+    public Rating(String kp) {
+        this.kp = kp;
+    }
+
+    public String getKp() {
+        return kp;
+    }
+
+    @Override
+    public String toString() {
+        return "Rating{" +
+                "kp='" + kp + '\'' +
+                '}';
+    }
+}
