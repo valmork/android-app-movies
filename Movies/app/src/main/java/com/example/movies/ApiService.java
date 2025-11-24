6f4f34e7793456b886c5a7efeed542267d6769ea
anton <korolyov.anton0208@gmail.com>
2025-11-24 10:57:56 +0300

    загрузка livedata

--- Movies/app/src/main/java/com/example/movies/ApiService.java
+++ Movies/app/src/main/java/com/example/movies/ApiService.java
@@ -2,9 +2,10 @@ package com.example.movies;
 
 import io.reactivex.rxjava3.core.Single;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface ApiService {
 
-    @GET("movie?token=S582NST-8P3434Q-NRRMRVD-HZWGV2M&field=rating.kp&search=7-10&sortField=votes.kp&sortType=-1&page=2&limit=40")
-    Single<MovieResponse> loadMovies();
+    @GET("movie?token=S582NST-8P3434Q-NRRMRVD-HZWGV2M&field=rating.kp&search=7-10&sortField=votes.kp&sortType=-1&limit=5")
+    Single<MovieResponse> loadMovies(@Query("page") int page);
 }
